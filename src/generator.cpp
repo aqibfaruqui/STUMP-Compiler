9ad0f71a297d10bceaed595f7504113f694f6848
aqibfaruqui <aqibfaruqui@gmail.com>
2025-03-23 18:01:00 +0000

    a brainstorm on how to handle generate function calls for functions that have not been declared yet

--- src/generator.cpp
+++ src/generator.cpp
@@ -9,6 +9,18 @@
     - after init stack, call generateMain() -> init registers and checks main exists 
     - call generateFunction() for func : functions
     - ...
+
+    known problems:
+        - if we generate main first, function calls from main must write missing function addresses to PC
+        - problem persists for any function that calls others
+        - therefore, pseudo topologically sorting our functions could let us write functions in order of calling
+            - problem of circular function calling? may be 'correct' if calls depend on conditional statements
+
+        - otherwise, let generator write functions with memory address left blank and let a second pass through fill them in
+            - how to edit std::stringstream on second pass through??
+
+        +++ treat like C, function calls only find functions declared above it!
+            - also does mean to generate main last
 */
 
 #include "generator.h"
@@ -50,6 +62,14 @@ void Generator::generateMain() {
     m_output << "MOV R4, #0\n";
     m_output << "MOV R5, #0\n";
     m_output << "LD SP, [R0, #stack]\n";
+
+    // func->name           ; ignore for main
+    // func->parameters     ; ignore for main
+    // func->body           ; generateBody()? body->statements
+
+    // for (auto& stmt : main->body) {
+    //     continue;
+    // }
 
     return;
 }
